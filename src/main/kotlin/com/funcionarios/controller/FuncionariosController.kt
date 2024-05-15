7d20f9db847751d3f0ad0a2a3c61291542a68bac
Thiago Lemes <thiago.lemes.nextage@gmail.com>
2024-05-15 10:44:40 -0300

    implementa o find e o find all

--- src/main/kotlin/com/funcionarios/controller/FuncionariosController.kt
+++ src/main/kotlin/com/funcionarios/controller/FuncionariosController.kt
@@ -5,20 +5,36 @@ import com.funcionarios.service.FuncionariosService
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
-import java.net.URI
+import java.util.*
 
 
 @RestController
 @RequestMapping("/funcionarios")
 class FuncionariosController(private val service: FuncionariosService) {
-
-    @PostMapping("/salva")
-    fun salvar(@RequestBody @Validated dto: FuncionarioDTO): ResponseEntity<FuncionarioDTO> {
-        service.salvar(dto)
-        return ResponseEntity.created(URI("/funcionarios/${dto.id}")).body(null)
+    @PostMapping("/create")
+    fun create(@RequestBody @Validated dto: FuncionarioDTO): ResponseEntity<FuncionarioDTO> {
+        service.create(dto)
+        return ResponseEntity.ok(dto)
     }
-    @DeleteMapping(value = ["/{id}"] )
-    fun delete(@PathVariable(value = "id") id: Long): ResponseEntity<*>{
+
+    @GetMapping(value = ["/{id}"])
+    fun findById(@PathVariable(value = "id") id: Long): Optional<FuncionarioDTO> {
+        return service.findById(id)
+    }
+
+    @GetMapping("find-all")
+    fun findAll(): List<FuncionarioDTO> {
+        return service.findAll()
+    }
+
+    @PutMapping("/update")
+    fun update(@RequestBody @Validated dto: FuncionarioDTO): ResponseEntity<Unit> {
+        service.update(dto)
+        return ResponseEntity.ok().build()
+    }
+
+    @DeleteMapping(value = ["/{id}"])
+    fun delete(@PathVariable(value = "id") id: Long): ResponseEntity<*> {
         service.delete(id)
         return ResponseEntity.noContent().build<Any>()
     }
--- src/main/kotlin/com/funcionarios/service/FuncionariosService.kt
+++ src/main/kotlin/com/funcionarios/service/FuncionariosService.kt
@@ -6,15 +6,66 @@ import com.funcionarios.repository.FuncionariosRepository
 import jakarta.transaction.Transactional
 import org.modelmapper.ModelMapper
 import org.springframework.stereotype.Service
+import java.util.*
+import kotlin.NoSuchElementException
 
 @Service
 class FuncionariosService(private val repository: FuncionariosRepository) {
     private val mapper: ModelMapper = ModelMapper() // Injeção do ModelMapper
+
     @Transactional
-    fun salvar(DTO: FuncionarioDTO) {
-        val entity = mapper.map(DTO, FuncionariosEntity::class.java)
+    fun create(dto: FuncionarioDTO) {
+        val entity = mapper.map(dto, FuncionariosEntity::class.java)
         repository.save(entity)
     }
+
+    fun findAll(): List<FuncionarioDTO> {
+        val funcionario = repository.findAll()
+        val funcionariosDTOs = funcionario.map { entity ->
+            val dto = FuncionarioDTO(
+                id = entity.id,
+                nome = entity.nome,
+                setor = entity.setor,
+                salario = entity.salario,
+                cargo = entity.cargo
+            )
+            dto
+        }
+        return funcionariosDTOs
+    }
+
+    fun findById(id: Long): Optional<FuncionarioDTO> {
+        val funcionarioEntityOptional = repository.findById(id)
+        return if (funcionarioEntityOptional.isPresent) {
+            val entity = funcionarioEntityOptional.get()
+            val dto = FuncionarioDTO(
+                id = entity.id,
+                nome = entity.nome,
+                setor = entity.setor,
+                salario = entity.salario,
+                cargo = entity.cargo
+            )
+            Optional.of(dto)
+        } else {
+            Optional.empty()
+        }
+    }
+
+
+    fun update(dto: FuncionarioDTO) {
+        if (repository.findById(dto.id).isEmpty || repository.findById(dto.id).equals(0)) {
+            throw NoSuchElementException("usuario não existe")
+        }
+        val update = mapper.map(dto, FuncionariosEntity::class.java)
+        update.apply {
+            nome = dto.nome
+            cargo = dto.cargo
+            setor = dto.setor
+            salario = dto.salario
+        }
+        repository.save(update)
+    }
+
     fun delete(id: Long) {
         val entity = repository.findById(id)
             .orElseThrow()
